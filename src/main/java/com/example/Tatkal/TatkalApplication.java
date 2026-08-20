8bba8f5eaa209cb09d0396020891192e2f846bc2
aryan <gargaryan12122005@gmail.com>
2026-08-20 15:08:33 +0530

    start the project

---
+++ src/main/java/com/example/Tatkal/TatkalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tatkal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatkalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatkalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tatkal/TatkalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tatkal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatkalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
